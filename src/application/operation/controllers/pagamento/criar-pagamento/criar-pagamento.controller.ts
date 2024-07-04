4db5572a7304181a4587e3485ef06fa0bb349075
Alexandre Fukano <alexandremkimura@hotmail.com>
2024-07-04 19:27:45 -0300

    fix: cyber issues

--- src/application/operation/controllers/pagamento/criar-pagamento/criar-pagamento.controller.ts
+++ src/application/operation/controllers/pagamento/criar-pagamento/criar-pagamento.controller.ts
@@ -1,6 +1,6 @@
-import { Inject } from '@nestjs/common';
+import { BadRequestException, Inject } from '@nestjs/common';
 import { PagamentosDtos } from '../../../../../core/pagamento/dto/cria-pagamento.dto';
-import { Pagamento } from '../../../../../core/pagamento/entity/pagamento.entity';
+import { Pagamento, PAGAMENTO_STATUS } from '../../../../../core/pagamento/entity/pagamento.entity';
 import { CriarPagamentoUseCase } from '../../../../../core/pagamento/usecase/criar-pagamento/criar-pagamento.usecase';
 
 export class CriarPagamentoController {
@@ -9,8 +9,44 @@ export class CriarPagamentoController {
     private criarPagamentoUseCase: CriarPagamentoUseCase,
   ) { }
 
-  async handle(pagamento: PagamentosDtos): Promise<Pagamento[]> {
-    const pagamentoCriado = await this.criarPagamentoUseCase.execute(pagamento);
+  private isUUID(str: string) {
+    const uuidRegex = /^[0-9a-f]{8}-?[0-9a-f]{4}-?4[0-9a-f]{3}-?[89ab][0-9a-f]{3}-?[0-9a-f]{12}$/i;
+
+    return uuidRegex.test(str);
+  }
+
+  private isValidEmail(email: string) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    return emailRegex.test(email);
+  }
+
+  private isValidISODate(dateString: string) {
+    const parsedDate = Date.parse(dateString);
+
+    return !isNaN(parsedDate);
+  }
+
+  async handle(pagamentos: PagamentosDtos): Promise<Pagamento[]> {
+
+    for (const pagamento of pagamentos.pagamentos) {
+      if (!this.isUUID(pagamento._id)) throw new BadRequestException('Wrong format')
+
+      const statusType = Object.keys(PAGAMENTO_STATUS)
+      if (!statusType.includes(pagamento.status)) throw new BadRequestException('Wrong format')
+
+      for (const pid of pagamento.produtosIds) {
+        if (!this.isUUID(pid)) throw new BadRequestException('Wrong format')
+      }
+
+      if (!this.isValidEmail(pagamento.clienteId)) throw new BadRequestException('Wrong format')
+
+      if (!this.isValidISODate(pagamento.createdAt) || !this.isValidISODate(pagamento.updatedAt)) throw new BadRequestException('Wrong format')
+    }
+
+    const pagamentoCriado = await this.criarPagamentoUseCase.execute(pagamentos);
+
+
     return pagamentoCriado;
   }
 }
--- src/core/pagamento/dto/cria-pagamento.dto.ts
+++ src/core/pagamento/dto/cria-pagamento.dto.ts
@@ -3,6 +3,11 @@ import { PAGAMENTO_STATUS } from '../entity/pagamento.entity';
 export type PagamentoDto = {
   _id: string;
   status: string;
+  produtosIds: string[];
+  clienteId: string;
+  createdAt: string;
+  updatedAt: string;
+  __v: number
 };
 
 export type PagamentosDtos = {
